83129e04aec8d795236627e4b0d76872450dc31d
Gaurav Walia <98510828+GWSafar@users.noreply.github.com>
2023-06-12 16:37:48 -0700

    Update go.js

--- go.js
+++ go.js
@@ -1,35 +1,30 @@
-// demo.js
 var fillInPage = (function() {
     var updateCityText = function(geoipResponse) {
-
-        /*
-         * It's possible that we won't have any names for this city.
-         * For language codes with a special character such as pt-BR,
-         * replace names.en with names['pt-BR'].
-        */
-        //var cityName = geoipResponse.city.names.en || 'your city';
-        var cityName = geoipResponse.represented_country.iso_code;
-        
-        
-        document.getElementById('city').innerHTML = cityName
+  
+      var cityName = geoipResponse.country.iso_code;
+  
+      document.getElementById('country').innerHTML = cityName
+      if (cityName == 'US') {
+        //document.getElementById('main').innerHTML = ''
+        document.getElementsByClassName("body lsc").innerHTML = ''
+      }
     };
-
+  
     var onSuccess = function(geoipResponse) {
-        updateCityText(geoipResponse);
+      updateCityText(geoipResponse);
     };
-
+  
     // If we get an error, we will display an error message
     var onError = function(error) {
-        document.getElementById('city').innerHTML = 'an error!  Please try again..'
+      document.getElementById('country').innerHTML = 'an error!  Please try again..'
     };
-
+  
     return function() {
-        if (typeof geoip2 !== 'undefined') {
-            geoip2.city(onSuccess, onError);
-        } else {
-            document.getElementById('city').innerHTML = 'a browser that blocks GeoIP2 requests'
-        }
+      if (typeof geoip2 !== 'undefined') {
+        geoip2.country(onSuccess, onError);
+      } else {
+      }
     };
-}());
-
-fillInPage();
+  }());
+  
+  fillInPage();
