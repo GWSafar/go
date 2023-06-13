46c060cb6d6edcacf3f6d021764a172117cb1b91
Gaurav Walia <98510828+GWSafar@users.noreply.github.com>
2023-06-12 18:20:17 -0700

    Update go.js

--- go.js
+++ go.js
@@ -16,7 +16,7 @@ var fillInPage = (function() {
   
     // If we get an error, we will display an error message
     var onError = function(error) {
-      document.querySelectorAll(".body.lsc").innerHTML = 'an error!  Please try again..'
+      document.getElementById("Section-1").innerHTML = 'an error!  Please try again..'
     };
   
     return function() {
